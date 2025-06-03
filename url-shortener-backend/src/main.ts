fab4b9f8dafda66c56d877d87fefd79ee54d5972
Fasilurahman <131866940+Fasilurahman@users.noreply.github.com>
2025-06-03 19:12:29 +0530

    Update main.ts

--- url-shortener-backend/src/main.ts
+++ url-shortener-backend/src/main.ts
@@ -7,9 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL||'https://url-shortly-liart.vercel.app/', 
-    credentials: true,               
-  });
+  origin: [
+    'https://url-shortly-liart.vercel.app',
+    'https://www.url-shortly-liart.vercel.app', 
+  ],
+  credentials: true,
+});
+
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
